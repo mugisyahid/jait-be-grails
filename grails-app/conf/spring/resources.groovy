898f9999e51d5318e437bb06f3881628ca7fff57
ligar mugi syahid <mugisyahid@gmail.com>
2018-01-18 23:27:08 +0700

    allow preflight request

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,5 +1,7 @@
+import com.jait.OptionRequestFilter
 import com.jait.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
     userPasswordEncoderListener(UserPasswordEncoderListener)
+    optionRequestFilter(OptionRequestFilter)
 }
